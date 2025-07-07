387b9422fae39cad3fc14487e0c78ab8a1c2d60f
sanyalyubenova <s.lyubenova13@gmail.com>
2025-07-07 23:00:12 +0300

    Updated models.py

--- InsuranceCompany/policies/models.py
+++ InsuranceCompany/policies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from InsuranceCompany.clients.models import Client
+
+
+class Policy(models.Model):
+    # policy_number = TO-DO
+    insurance_value = models.IntegerField()
+    insurance_price = models.FloatField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    client = models.ForeignKey(
+        Client,
+        on_delete=models.CASCADE,
+        editable=False,
+    )
