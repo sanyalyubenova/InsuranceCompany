851f27721143487569eead3f4a0cd0a4ceb421c3
sanyalyubenova <s.lyubenova13@gmail.com>
2025-07-26 23:09:53 +0300

    Updated models.py, views.py and forms.py

--- InsuranceCompany/accounts/models.py
+++ InsuranceCompany/accounts/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth import models as auth_models,get_user_model
 from django.db import models
+from InsuranceCompany.accounts.managers import AppUserManager
 
 # Create your models here.
+
+class AppUser(auth_models.AbstractBaseUser, auth_models.PermissionsMixin):
+    email = models.EmailField(unique=True, null=False, blank=False)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = AppUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+UserModel = get_user_model()
+
+class Profile(models.Model):
+    user = models.OneToOneField(UserModel, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(max_length=30, null= True, blank=True)
+    last_name = models.CharField(max_length=30, null= True, blank=True)
+    date_of_birth = models.DateField(null= True, blank=True)
+
+    def get_profile_name(self):
+        if self.first_name and self.last_name:
+            return f'{self.first_name} {self.last_name}'
+        elif self.first_name or self.last_name:
+            return self.first_name or self.last_name
+        else:
+            return 'Anonymous user'
--- InsuranceCompany/accounts/views.py
+++ InsuranceCompany/accounts/views.py
@@ -1,27 +1,58 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+from django.views import generic as views
+from django.views.generic import DetailView, DeleteView
 
+from InsuranceCompany.accounts.forms import AppUserCreationForm, AppUserLoginForm, ProfileEditForm
+
+UserModel = get_user_model()
 
 # Create your views here.
 
-def register(request):
-    return render(request, 'accounts/register.html')
+class AppUserRegisterView(views.CreateView):
+    model = UserModel
+    form_class = AppUserCreationForm
+    template_name = 'accounts/register.html'
+    success_url = reverse_lazy('login')
 
 
-def login(request):
-    return render(request, 'accounts/login.html')
+class AppUserLoginView(LoginView):
+    form_class = AppUserLoginForm
+    template_name = 'accounts/login.html'
 
 
 def logout(request):
     return render(request, 'accounts/logout.html')
 
 
-def profile_details(request):
-    return render(request, 'accounts/profile_details.html')
+class AppUserDetailView(DetailView):
+    model = UserModel
+    template_name = 'accounts/profile_details.html'
 
 
-def edit_profile(request):
-    return render(request, 'accounts/profile_edit.html')
+class ProfileEditView(views.UpdateView):
+    model = UserModel
+    form_class = ProfileEditForm
+    template_name = 'accounts/profile_edit.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user.profile
+
+    def get_success_url(self):
+        return reverse_lazy('profile-details', kwargs={'pk': self.object.pk})
 
 
-def delete_profile(request):
-    return render(request, 'accounts/profile_delete.html')
+class AppUserDeleteView(DeleteView):
+    model = UserModel
+    template_name = 'accounts/profile_delete.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def delete(self, request, *args, **kwargs):
+        user = self.get_object()
+        user.delete()
+        return redirect(self.get_success_url())
