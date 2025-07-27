efb85a9ecbd6ec509d68c829e496d3a370c160cc
sanyalyubenova <146644433+sanyalyubenova@users.noreply.github.com>
2025-07-27 16:43:45 +0300

    Delete InsuranceCompany/staff directory

--- InsuranceCompany/staff/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Staff(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
--- InsuranceCompany/staff/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('dashboard/', views.staff_dashboard, name='client_dashboard')
-]
