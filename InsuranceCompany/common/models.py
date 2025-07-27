1538235be753f6fe4a9b50a74520a971ff5145e1
sanyalyubenova <146644433+sanyalyubenova@users.noreply.github.com>
2025-07-27 16:42:47 +0300

    Update models.py

--- InsuranceCompany/common/models.py
+++ InsuranceCompany/common/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+from InsuranceCompany.policies.models import Policy
+
+
 # Create your models here.
+
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    price = models.IntegerField()
+    policy = models.ForeignKey(
+        Policy,
+        on_delete=models.CASCADE,
+        editable=False,
+    )
+
+
+
+
