550a19f44a84841fd2aa4c6564e14b4e02feabdd
sanyalyubenova <146644433+sanyalyubenova@users.noreply.github.com>
2025-07-27 16:40:02 +0300

    Update models.py

--- InsuranceCompany/accounts/models.py
+++ InsuranceCompany/accounts/models.py
@@ -1,3 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
 from django.contrib.auth import models as auth_models,get_user_model
 from django.db import models
 from InsuranceCompany.accounts.managers import AppUserManager
