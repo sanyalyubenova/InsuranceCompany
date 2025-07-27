959588d5f30ba783ffb7bc3b775e8600c8d513b7
sanyalyubenova <146644433+sanyalyubenova@users.noreply.github.com>
2025-07-27 16:42:24 +0300

    Delete InsuranceCompany/clients directory

--- InsuranceCompany/clients/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Client(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
--- InsuranceCompany/clients/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def client_dashboard(request):
-    pass
