e6759c74e08819993cffac4f86c1fe3b7a048d5b
sanyalyubenova <146644433+sanyalyubenova@users.noreply.github.com>
2025-07-30 22:29:22 +0300

    Update views.py

--- InsuranceCompany/policies/views.py
+++ InsuranceCompany/policies/views.py
@@ -1,21 +1,41 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from InsuranceCompany.policies.forms import PolicyForm
+from InsuranceCompany.policies.models import InsurancePolicy
+
 
 # Create your views here.
-def policy_create(request):
-    return None
 
 
-def policy_details(request):
-    return None
+class PolicyListView(ListView):
+    model = InsurancePolicy
+    template_name = 'policies/policy_list.html'
+    context_object_name = 'policies'
+    paginate_by = 10
 
 
-def policy_edit(request):
-    return None
+class PolicyDetailView(DetailView):
+    model = InsurancePolicy
+    template_name = 'policies/policy_detail.html'
 
 
-def policy_delete(request):
-    return None
+class PolicyCreateView(CreateView):
+    model = InsurancePolicy
+    form_class = PolicyForm
+    template_name = 'policies/policy_create.html'
+    success_url = reverse_lazy('policy_list')
 
 
-def calculator(request):
-    return None
+class PolicyUpdateView(UpdateView):
+    model = InsurancePolicy
+    form_class = PolicyForm
+    template_name = 'policies/policy_update.html'
+
+
+class PolicyDeleteView(DeleteView):
+    model = InsurancePolicy
+    success_url = reverse_lazy('policy_list')
+
+
